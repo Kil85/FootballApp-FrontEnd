cdde8ebceeddd1a0b19c08d18b5055fe37f31138
Kamil Skupień <kil85@interia.pl>
2023-12-12 23:19:44 +0100

    base auth added

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,125 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, throwError } from 'rxjs';
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from '../shared/user.model';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  baseLink = 'https://localhost:7183/api/account/';
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(mail: string, password: string) {
+    return this.http
+      .post(
+        this.baseLink + 'login',
+        {
+          email: mail,
+          password: password,
+        },
+        {
+          responseType: 'text',
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  handleJwt(jwt: string) {
+    const helper = new JwtHelperService();
+
+    const decodedToken = helper.decodeToken(jwt);
+    const expirationDate = new Date(
+      new Date().getTime() + +decodedToken.exp * 1000
+    );
+
+    const nameIdentifier =
+      decodedToken[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'
+      ];
+    const role =
+      decodedToken[
+        'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+      ];
+    const email =
+      decodedToken[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
+      ];
+
+    const user = new User(email, nameIdentifier, role, jwt, expirationDate);
+    this.user.next(user);
+    this.autoLogout(decodedToken.exp);
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      role: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData.role,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    console.log(errorRes);
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Observable, Subscription } from 'rxjs';
+
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  user: User;
+  isLogged = false;
+  private userSub: Subscription;
 
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSub = this.auth.user.subscribe((u) => {
+      this.user = u;
+      this.isLogged = !!u;
+    });
+  }
+
+  onSettings() {}
+  onLogout() {
+    this.auth.logout();
+  }
+  onRegister() {}
+  onLogin() {
+    let obs = new Observable();
+    obs = this.auth.login('string', 'string');
+    obs.subscribe((data: string) => {
+      this.auth.handleJwt(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
